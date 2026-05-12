6d57e2babc890fe3e29bace8bbd90459ec82e105
Nickolas Xisto Machado <xistonickolas@gmail.com>
2026-05-12 13:19:12 -0300

    dataset cordataset corrigido

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import json
 import torch
-from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
 from sklearn.preprocessing import MultiLabelBinarizer
 
 from transformers import Trainer, TrainingArguments
 from sklearn.model_selection import train_test_split
+
 with open('card_dataset.json', encoding='utf-8') as f:
     tmp =  json.load(f)
 
-
 df = pd.DataFrame.from_dict(tmp, orient='index')
-df = df.reset_index().rename(columns={'index': 'card_name'}) #switch the index to a number instead of the card's name
+#df = df.reset_index().rename(columns={'index': 'card_name'}) #switch the index to a number instead of the card's name
 
-df = df[df["tags"].apply(lambda x: isinstance(x, list))]
-
+#df = df[df["tags"].apply(lambda x: isinstance(x, list))]
+#print(df['tags'].explode().value_counts())
+#a  = df['tags'].explode().value_counts()
+#b = a[a ==2]
+#print(b)
+print(df['tags'].unique())
+'''
 mlb = MultiLabelBinarizer()
 Y = mlb.fit_transform(df["tags"])
 
@@ -26,3 +30,4 @@ for train_idx, val_idx in msss.split(df, Y):
     X_train, X_val = df.iloc[train_idx], df.iloc[val_idx]
     Y_train, Y_val = Y[train_idx], Y[val_idx]
     
+'''
